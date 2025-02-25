7e34c9a697efcd61f4132dd385a4ae0a33891985
CodeBusuki <nakulchauhan2017@gmail.com>
2025-02-25 17:40:20 +0530

    Update vite.config.js

--- Loopstudios-landing-page/vite.config.js
+++ Loopstudios-landing-page/vite.config.js
@@ -3,4 +3,4 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],base:"./Frontend-Projects",})
+  plugins: [react()],base:"/Frontend-Projects",})
